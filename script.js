38c959abd62559ee419933d55b192f1d71a421e1
thanuja172 <thanujaalaguraja95@gmail.com>
2025-04-15 21:33:48 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,8 @@
+document.querySelectorAll('nav a').forEach(link => {
+    link.addEventListener('click', event => {
+      event.preventDefault();
+      const section = document.querySelector(link.getAttribute('href'));
+      section.scrollIntoView({ behavior: 'smooth' });
+    });
+  });
+  
